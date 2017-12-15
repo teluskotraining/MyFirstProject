f7b4e04cd0367ec01346b31243d8f25187e82e1c
Rabin Karki <rabinfogithub@gmail.com>
2017-12-15 15:45:03 +0545

    Updated Calc and Runner

--- MyFirstProject/src/com/telusko/Calculator.java
+++ MyFirstProject/src/com/telusko/Calculator.java
@@ -1,14 +1,12 @@
 package com.telusko;
 
 // Calculator Class
-public class Calculator 
-{
-	public int add(int i,int j)
-	{
-		return i+j;
+public class Calculator {
+	public int add(int i, int j) {
+		return i + j;
 	}
-	public int sub(int i,int j)
-	{
-		return i-j;
+
+	public int mult(int i, int j) {
+		return i * j;
 	}
 }
